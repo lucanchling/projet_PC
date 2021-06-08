5f8cd760004a2c314915fb550ebdc46e28cf4df9
Teklaw7 <timothee.teyssier@cpe.fr>
2021-06-08 20:39:05 +0200

    affichage grille début

--- game_of_life.py
+++ game_of_life.py
@@ -1,10 +1,33 @@
 import multiprocessing as mp
+from random import randint
+import time
+import os
 
-CLEARSCR="\x1B[2J\x1B[;H"        #  Clear SCReen
-CLEAREOS = "\x1B[J"                #  Clear End Of Screen
-CLEARELN = "\x1B[2K"               #  Clear Entire LiNe
-CLEARCUP = "\x1B[1J"               #  Clear Curseur UP
-GOTOYX   = "\x1B[%.2d;%.2dH"       #  Goto at (y,x), voir le code
 
 CL_WHITE="\033[01;37m"                  #  Blanc
 CL_BLACK="\033[22;30m"                  #  Noir
+CLEARSCR="\x1B[2J\x1B[;H"        #  Clear SCReen
+
+nb_proc=4
+long_horiz=20
+nb_cellule=long_horiz**2
+def affiche_Grille(taille_Grille,Grille):
+    os.system('clear')
+    for i in range(taille_Grille):
+        for j in range(taille_Grille):
+            if [j+(i*taille_Grille)]==0:
+                couleur = CL_WHITE
+            else:
+                couleur = CL_BLACK
+            color(couleur)
+        print(' A ',end='')
+
+
+
+def color(couleur):
+    print(couleur,end='')
+
+
+grille=mp.Array('i',[randint(0,1) for i in range(nb_cellule)])
+
+affiche_Grille(long_horiz,grille)

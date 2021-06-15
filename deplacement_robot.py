666e4219ddd518908386bf5cefee7749815ad167
lucanchling <luc.anchling@cpe.fr>
2021-06-15 09:29:52 +0200

    Robot - Position Robot OK

--- deplacement_robot.py
+++ deplacement_robot.py
@@ -93,6 +93,7 @@ class Case(Enum):
 #------------------------------------------------
 temps_Ecran = Value('i',1) # Initialisation du temps pour la fonction écran
 temps_Ir = Value('i',1) # Initialisation du temps pour la fonction ir
+temps_Controleur = Value('i',1) # Pour le controleur
 #------------------------------------------------
 
 #################
@@ -113,35 +114,63 @@ def grille(n):
         j = random.randint(0,n-1)
         grille[i][j]=Case.Obstacle.value
     # Positionnement du robot sur une case vide
-    i,j=n//2,n//2
+    i,j=random.randint(0,taille_grille-1),n//2
     while grille[i][j] == Case.Obstacle.value:
-        i+=1
-        j+=1
+        i=(i+1)%taille_grille  # Modulo pour éviter un out_of_bounds
+        j=(j+1)%taille_grille
     grille[i][j] = Case.Robot.value
 
     return grille
 
 # Permet d'utiliser conjointement les différents capteurs afin de contrôler le robot 
 def controleur():
-    return False
+    tic = time.time()
+    while True:
+        # Pour le faire toutes les temps_Ecran secondes
+        if (tic-time.time())%temps_Controleur.value == 0:
+            Commande = Cmd.Front.value
+            Flag = False
+            Verrou.acquire()
+            mem_Cmd[indice] = Commande
+            mem_Flag[indice] = Flag   
+            Verrou.release()
 
 # Permet l'affichage sur l'écran (I suppose)
 def ecran(n):
     print(grille(n))
     tic = time.time()
-    while True:
-        # Pour le faire toutes les temps_Ecran secondes
-        if (tic-time.time())%temps_Ecran == 0:    
-            Verrou.acquire()
-            # Ici : gestion de l'affichage du déplacement
-            Verrou.release()
+    # while True:
+    #     # Pour le faire toutes les temps_Ecran secondes
+    #     if (tic-time.time())%temps_Ecran.value == 0:
+
+    #         Verrou.acquire()
+    #         # if mem_Cmd[indice] == Cmd.Front.value:
+    #         #     #Pour avancer
+    #         #     print()
+    #         # if mem_Cmd[indice] == Cmd.Left.value:
+    #         #     #Pour aller à droite
+    #         #     print()
+    #         # else :
+    #         #     #Pour aller à gauche
+    #         #     print()
+
+    #         Verrou.release()
+
+# Permet de rechercher la position du robot dans la grille
+def position_robot():
+    grille = sa.attach("shm://grille")
+    for ligne in range(0,taille_grille):
+        for colonne in range(0,taille_grille):
+            if grille[ligne][colonne] == Case.Robot.value:
+                Robot_X.value = ligne
+                Robot_Y.value = colonne
 
 # Permet la gestion des capteurs (Left & Right)
 def ir():
     tic = time.time()
     while True :
         # Pour le faire toutes les temps_Ir secondes
-        if (tic-time.time())%temps_Ir == 0:    
+        if (tic-time.time())%temps_Ir.value == 0:    
             return False
 
 # Permet la gestion du capteur (US = Front)
@@ -158,20 +187,24 @@ if __name__ == "__main__" :
     # Déclaration des différentes Variables :
     
     Verrou = Lock()
+
+    # Position du robot :
+    Robot_X = Value('i',0)
+    Robot_Y = Value('i',0)
     # Les différentes distances :
     Dist_Front = Value('d',0.0)
     Dist_Left = Value('d',0.0)
     Dist_Right = Value('d',0.0)
-    # Les différents drapeaux
+    # Les différents drapeaux :
     Flag_Front = Value('b',False)
     Flag_Right = Value('b',False)
     Flag_Left = Value('b',False)
     # Les mémoires partagées :
-    indice = Value('i',0) # Indices permettant la numérotation aisin que l'écriture des commandes
+    indice = Value('i',0) # Indices permettant la numérotation ainsi que l'écriture des commandes
     mem_Cmd = Array('i',[0 for i in range(100000)])  # tableau partagé des commandes
     mem_Flag = Array('i',[0 for i in range(100000)])  # Tableau partagé des drapeaux
-
-    taille_grille = 10
+    
+    taille_grille = 5
     # while taille_grille == 0:
     #     try :
     #         taille_grille = int(input("Taille de la grille carrée : "))
@@ -179,5 +212,8 @@ if __name__ == "__main__" :
     #         print("Saisir un entier !! ")
     
     Process_Ecran = Process(target=ecran, args=(taille_grille,))
+    Process_position = Process(target=position_robot)
+
     Process_Ecran.start()
-    
+    time.sleep(1)
+    Process_position.start()
